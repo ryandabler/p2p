80efdb50002088aaa89753e221f77618b32b4c7c
Ryan Dabler <rpdabler@gmail.com>
2018-05-04 14:21:43 -0600

    create Header component

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./header.css";
+
+export default function Header(props) {
+    return (
+        <header className="header">
+            P2P
+        </header>
+    );
+}
