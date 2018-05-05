6d4e5de66391deafc672922756713a54d5a55c8d
Ryan Dabler <rpdabler@gmail.com>
2018-05-04 20:30:26 -0600

    cnvt Sidebar items to links

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,17 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./sidebar.css";
 
 export default function SideBar(props) {
     return (
         <div className="sidebar">
-            <div>Vendors</div>
-            <div>Items</div>
-            <div>Contracts</div>
-            <div>Invoices</div>
-            <div>Containers</div>
-            <div>Shipments</div>
-            <div>Payments</div>
+            <Link to={"/vendors"} className="plain-link">Vendors</Link>
+            <Link to={"/items"} className="plain-link">Items</Link>
+            <Link to={"/contracts"} className="plain-link">Contracts</Link>
+            <Link to={"/invoices"} className="plain-link">Invoices</Link>
+            <Link to={"/containers"} className="plain-link">Containers</Link>
+            <Link to={"/shipments"} className="plain-link">Shipments</Link>
+            <Link to={"/payments"} className="plain-link">Payments</Link>
         </div>
     );
 }
