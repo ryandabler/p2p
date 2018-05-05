c3aaa321369172a2b2ba8467714c07adab961221
Ryan Dabler <rpdabler@gmail.com>
2018-05-04 20:28:48 -0600

    Implement Router for different App paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
 import Header from "./components/header";
@@ -8,11 +8,22 @@ import SideBar from "./components/sidebar";
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
         <Header />
         <SideBar />
-        <div>middle</div>
+          <Switch>
+            <Route exact path={"/"} render={() => <div>Root</div>} />
+            <Route exact path={"/vendors"} render={() => <div>Vendors</div>} />
+            <Route exact path={"/items"} render={() => <div>Items</div>} />
+            <Route exact path={"/contracts"} render={() => <div>Contracts</div>} />
+            <Route exact path={"/invoices"} render={() => <div>Invoices</div>} />
+            <Route exact path={"/containers"} render={() => <div>Containers</div>} />
+            <Route exact path={"/shipments"} render={() => <div>Shipments</div>} />
+            <Route exact path={"/payments"} render={() => <div>Payments</div>} />
+          </Switch>
       </div>
+      </Router>
     );
   }
 }
