fbea8b802b96e6b85b10f18be77534125295f832
Ryan Dabler <rpdabler@gmail.com>
2018-05-04 21:29:41 -0600

    prop type validation

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PropTypes } from "prop-types";
 
 import { sideBarLinks } from "../config";
 
@@ -20,4 +21,8 @@ export default function SideBar(props) {
             {links}
         </div>
     );
+}
+
+SideBar.propTypes = {
+    location: PropTypes.object
 }
