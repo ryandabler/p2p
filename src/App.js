6fdb83c4d650dddaac45fd5d0dc0f21f81e1168d
Ryan Dabler <rpdabler@gmail.com>
2018-05-04 20:30:10 -0600

    indentation

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ import Header from "./components/header";
 import SideBar from "./components/sidebar";
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-      <div className="App">
-        <Header />
-        <SideBar />
-          <Switch>
-            <Route exact path={"/"} render={() => <div>Root</div>} />
-            <Route exact path={"/vendors"} render={() => <div>Vendors</div>} />
-            <Route exact path={"/items"} render={() => <div>Items</div>} />
-            <Route exact path={"/contracts"} render={() => <div>Contracts</div>} />
-            <Route exact path={"/invoices"} render={() => <div>Invoices</div>} />
-            <Route exact path={"/containers"} render={() => <div>Containers</div>} />
-            <Route exact path={"/shipments"} render={() => <div>Shipments</div>} />
-            <Route exact path={"/payments"} render={() => <div>Payments</div>} />
-          </Switch>
-      </div>
-      </Router>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Header />
+					<SideBar />
+					<Switch>
+						<Route exact path={"/"} render={() => <div>Root</div>} />
+						<Route exact path={"/vendors"} render={() => <div>Vendors</div>} />
+						<Route exact path={"/items"} render={() => <div>Items</div>} />
+						<Route exact path={"/contracts"} render={() => <div>Contracts</div>} />
+						<Route exact path={"/invoices"} render={() => <div>Invoices</div>} />
+						<Route exact path={"/containers"} render={() => <div>Containers</div>} />
+						<Route exact path={"/shipments"} render={() => <div>Shipments</div>} />
+						<Route exact path={"/payments"} render={() => <div>Payments</div>} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
