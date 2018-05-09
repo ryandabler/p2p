295249b3f9f4fd40f74fc7e8561299af3377432b
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 12:31:58 -0600

    initialize state in reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,13 @@
 import {} from "./actions";
 
-const initialState = {}
+const initialState = {
+    vendors: [],
+    items: [],
+    contracts: [],
+    invoices: [],
+    containers: [],
+    shipments: []
+}
 
 export const reducer = (state = initialState, action) => {
     return state;
