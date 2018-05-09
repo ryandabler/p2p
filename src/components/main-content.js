3009710bc313b34a79bfaa64ccdbb2e76557e826
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 10:51:16 -0600

    retrieve kpis from config.js

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 import MiniDash from "./mini-dash";
+import { kpis } from "../config";
+import { getLastURLSegment } from "../utilities";
 
 import "./main-content.css";
 
-export default function MainContent() {
+export default function MainContent(props) {
+    const _kpis = kpis[getLastURLSegment(props.match.url)];
+    
     return (
         <div className="main-content">
             <MiniDash />
