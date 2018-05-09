f445fd223fe24e2b6c74622ef2aebdc8b1572cac
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 10:11:29 -0600

    add mini dashboard

--- src/components/main-content.js
+++ src/components/main-content.js
@@ -4,6 +4,13 @@ import "./main-content.css";
 
 export default function MainContent() {
     return (
-        <div className="main-content"> Main content </div>
+        <div className="main-content">
+            <div className="mini-dash">
+                <div className="kpi">
+                    <div className="kpi-text">Open contracts</div>
+                    <div className="kpi-value">5</div>
+                </div>
+            </div>
+        </div>
     );
 }
