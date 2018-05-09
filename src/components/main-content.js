48759d6d4c95d39e4420c901ed2688b650aee1e3
Ryan Dabler <rpdabler@gmail.com>
2018-05-08 21:22:58 -0600

    create MainContent component

---
+++ src/components/main-content.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./main-content.css";
+
+export default function MainContent() {
+    return (
+        <div className="main-content"> Main content </div>
+    );
+}
