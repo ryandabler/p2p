9c79f479b20b80c971ea77c7b44f1ff26679d47a
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 10:43:13 -0600

    switch obtaining url segment to getLastURLSegment

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 import { PropTypes } from "prop-types";
 
 import { sideBarLinks } from "../config";
+import { getLastURLSegment } from "../utilities";
 
 import "./sidebar.css";
 
 export default function SideBar(props) {
     const links = sideBarLinks.map(linkTxt => {
-        const match = props.location.pathname.split("/")[1] === linkTxt ? true : false;
+        const match = getLastURLSegment(props.location.pathname) === linkTxt ? true : false;
         return (
             <Link key={linkTxt} to={`/${linkTxt}`} className={match ? "plain-link link-match" : "plain-link"}>
                 {linkTxt}
