7baa7d2275002014be8670b0ba9f5794e181bc28
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 12:13:55 -0600

    create list of db stores

--- src/config.js
+++ src/config.js
@@ -18,3 +18,27 @@ export const kpis = {
 
 export const DB_NAME = "P2P";
 export const DB_VERSION = 1;
+export const DB_STORES = [
+    {
+        name: "vendors",
+        options: { keyPath: "name" }
+    }, {
+        name: "items",
+        options: { keyPath: "name" }
+    }, {
+        name: "contracts",
+        options: { keyPath: "id" }
+    }, {
+        name: "invoices",
+        options: { keyPath: [ "vendor", "invoiceId" ] }
+    }, {
+        name: "containers",
+        options: { keyPath: "containerId" }
+    }, {
+        name: "shipments",
+        options: { autoIncrement: true }
+    }, {
+        name: "payments",
+        options: { autoIncrement: true }
+    }, 
+];
