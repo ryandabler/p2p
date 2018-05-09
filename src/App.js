91355dbfece10dcb47dcfbada42ecf4bd4c3e8ab
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 12:43:05 -0600

    all paths should render MainContent

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 
 import Header from "./components/header";
 import SideBar from "./components/sidebar";
+import MainContent from "./components/main-content";
 import { getEntireDB } from "./indexeddb";
 import { loadData } from "./actions";
 
@@ -27,14 +28,14 @@ class App extends Component {
 					<Header />
 					<Route path="/" component={SideBar} />
 					<Switch>
-						<Route exact path={"/"} render={() => <div>Root</div>} />
-						<Route exact path={"/vendors"} render={() => <div>Vendors</div>} />
-						<Route exact path={"/items"} render={() => <div>Items</div>} />
-						<Route exact path={"/contracts"} render={() => <div>Contracts</div>} />
-						<Route exact path={"/invoices"} render={() => <div>Invoices</div>} />
-						<Route exact path={"/containers"} render={() => <div>Containers</div>} />
-						<Route exact path={"/shipments"} render={() => <div>Shipments</div>} />
-						<Route exact path={"/payments"} render={() => <div>Payments</div>} />
+						<Route exact path={"/"} render={props => <MainContent match={props.match} />} />
+						<Route exact path={"/vendors"} render={props => <MainContent match={props.match} />} />
+						<Route exact path={"/items"} render={props => <MainContent match={props.match} />} />
+						<Route exact path={"/contracts"} render={props => <MainContent match={props.match} />} />
+						<Route exact path={"/invoices"} render={props => <MainContent match={props.match} />} />
+						<Route exact path={"/containers"} render={props => <MainContent match={props.match} />} />
+						<Route exact path={"/shipments"} render={props => <MainContent match={props.match} />} />
+						<Route exact path={"/payments"} render={props => <MainContent match={props.match} />} />
 					</Switch>
 				</div>
 			</Router>
