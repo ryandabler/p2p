93cf730d10caabe0e63536d6a77fed5a9d217f57
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 20:08:55 -0600

    implement thead from columns prop

--- src/components/table.js
+++ src/components/table.js
@@ -2,10 +2,16 @@ import React from "react";
 
 import "./table.css";
 
-export default function Table() {
+export default function Table(props) {
+    const header = props.columns.map(column => <th key={column}>{column}</th>);
+
     return (
         <table>
-            
+            <thead>
+                <tr>
+                    {header}
+                </tr>
+            </thead>
         </table>
     )
 }
