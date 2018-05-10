270ab3f56396879945129543c7c140ce53471847
Ryan Dabler <rpdabler@gmail.com>
2018-05-09 19:55:29 -0600

    create initial transform mechanism for data

--- src/utilities.js
+++ src/utilities.js
@@ -2,3 +2,19 @@ export function getLastURLSegment(url) {
     const segments = url.split("/");
     return segments[segments.length - 1];
 }
+
+export function transformContracts(data) {
+    return data.map(contract => {
+        return {
+            contract: contract.id,
+            vendor: contract.vendor,
+            amount: contract.items.reduce((accum, item) => accum + item.amount),
+            deposit: contract.deposit,
+            closed: contract.items.reduce((accum, item) => accum && item.closed)
+        };
+    });
+}
+
+export const transformRules = {
+    contracts: transformContracts
+};
