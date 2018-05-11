890a71bcdb6094e1a1b297eff7d3a62f5ba40842
Ryan Dabler <rpdabler@gmail.com>
2018-05-10 19:57:33 -0600

    Revert "parametrize path for SideBar"
    This reverts commit c695a903e9910ecea974d204c2c36ff878cad32e.

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 			<Router>
 				<div className="App">
 					<Header />
-					<Route path="/:component" component={SideBar} />
+					<Route path="/" component={SideBar} />
 					<Switch>
 						<Route exact path={"/"} render={props => <MainContent match={props.match} />} />
 						<Route exact path={"/vendors"} render={props => <MainContent match={props.match} />} />
