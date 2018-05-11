48c58b522a28dec715f491184d674ef000ebe28a
Ryan Dabler <rpdabler@gmail.com>
2018-05-11 09:49:36 -0600

    create NewContractLineItem component

---
+++ src/components/new-contract-line-item.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { addNewFormLine } from "../actions";
+
+import "./new-contract-line-item.css";
+
+export function NewContractLineItem(props) {
+    function addNewLine(e) {
+        e.preventDefault();
+        const fields = {
+            product: "",
+            price: "",
+            quantity: ""
+        };
+        props.dispatch(addNewFormLine(fields));
+    }
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.form.map((line, idx) =>
+                    <tr key={idx}>
+                        <td><input defaultValue={line.product} /></td>
+                        <td><input defaultValue={line.quantity} /></td>
+                        <td><input defaultValue={line.price} /></td>
+                    </tr>
+                )}
+                <tr>
+                    <td colSpan="3"><button onClick={addNewLine} className="buttonize clr-green">New</button></td>
+                </tr>
+            </tbody>
+        </table>
+    );
+}
+
+const mapStateToProps = state => ({
+    form: state.form
+});
+
+export default connect(mapStateToProps)(NewContractLineItem);
