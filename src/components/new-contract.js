2e3d3e5e35fe84d528c1a0746e71228e70172fd5
Ryan Dabler <rpdabler@gmail.com>
2018-05-11 09:16:01 -0600

    create initial form

--- src/components/new-contract.js
+++ src/components/new-contract.js
@@ -3,5 +3,18 @@ import React from "react";
 import "./new-contract.css";
 
 export default function NewContract() {
-
+    return (
+        <div className="new-contract">
+            <form className="new-contract-form">
+                <label htmlFor="contractId">Contract</label>
+                <input id="contractId" name="id" />
+                <label htmlFor="vendorName">Vendor</label>
+                <input id="vendorName" name="vendor" />
+                <label htmlFor="contractDate">Date</label>
+                <input id="contractDate" name="date" />
+                <label htmlFor="contractDeposit">Deposit</label>
+                <input id="contractDeposit" name="deposit" />
+            </form>
+        </div>
+    )
 }
