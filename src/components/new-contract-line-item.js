0cd4d44dbe14f346020c0637e914c0b0001b5d5b
Ryan Dabler <rpdabler@gmail.com>
2018-05-15 08:53:37 -0600

    abstractify NewContractLineItem logic

--- src/components/new-contract-line-item.js
+++ src/components/new-contract-line-item.js
@@ -1,26 +1,38 @@
 import React from "react";
 
+import { newFormFields } from "../config";
+
 import "./new-contract-line-item.css";
 
 export default function NewContractLineItem(props) {
+    const headers = newFormFields[props.type].items.map(item => 
+        <th key={item.header}>{item.header}</th>
+    );
+
+    const rows = props.form.map((line, idx) => {
+        const tds = newFormFields[props.type].items.map(item => (
+            <td key={`${idx}-${item.name}`}>
+                <input defaultValue={line[item.name]} name={`${idx}-${item.name}`} required={item.required} />
+            </td>
+        ));
+        
+        return (
+            <tr key={idx}>
+                {tds}
+                <td><span className="x linkify">×</span></td>
+            </tr>
+        );
+    });
+
     return (
         <table className="new-contract-line-items">
             <thead>
                 <tr>
-                    <th>Product</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
+                    {headers}
                 </tr>
             </thead>
             <tbody>
-                {props.form.map((line, idx) =>
-                    <tr key={idx}>
-                        <td><input defaultValue={line.product} name={`${idx}-product`} required /></td>
-                        <td><input defaultValue={line.quantity} name={`${idx}-quantity`} required /></td>
-                        <td><input defaultValue={line.price} name={`${idx}-price`} required /></td>
-                        <td><span className="x linkify">×</span></td>
-                    </tr>
-                )}
+                {rows}
             </tbody>
         </table>
     );
