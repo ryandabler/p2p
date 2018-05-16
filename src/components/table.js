fd2dacd583506e7eb6cbc99310349df70f76c9c1
Ryan Dabler <rpdabler@gmail.com>
2018-05-15 20:46:15 -0600

    generate Table content

--- src/components/table.js
+++ src/components/table.js
@@ -4,7 +4,18 @@ import "./table.css";
 
 export default function Table(props) {
     const header = props.columns.map(column => <th key={column}>{column}</th>);
+    const rows = props.data.map((row, idx) => {
+        const cells = props.columns.map((column, _idx) => (
+            <td key={`${idx}-${_idx}`}>{row[column.toLowerCase()]}</td>
+        ));
 
+        return (
+            <tr key={idx}>
+                {cells}
+            </tr>
+        );
+    });
+    
     return (
         <table className="data-table">
             <thead>
@@ -12,6 +23,9 @@ export default function Table(props) {
                     {header}
                 </tr>
             </thead>
+            <tbody>
+                {rows}
+            </tbody>
         </table>
     )
 }
